4b4727a0f887492e1f749bd029e224a57fbc07aa
Furkan Sarı <furkansa@hotmail.com.tr>
2025-12-05 22:28:14 +0300

    feat: add forgotten table to display the customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,29 @@
-import { Metadata } from "next";
+import { Suspense } from "react";
+import CustomersTable from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { fetchFilteredCustomers } from "@/app/lib/data";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string };
+}) {
+  const query = searchParams?.query || "";
+
+  return (
+    <div className="w-full">
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+        <CustomersWrapper query={query} />
+      </Suspense>
+    </div>
+  );
+}
+
+async function CustomersWrapper({ query }: { query: string }) {
+  const customers = await fetchFilteredCustomers(query);
+  return <CustomersTable customers={customers} />;
 }
